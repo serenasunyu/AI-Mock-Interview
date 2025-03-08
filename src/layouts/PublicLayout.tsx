cea5c96836f64b01704c874ef4dcc777c395e73e
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2025-03-08 15:21:09 -0500

    firebase configuration

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,12 +1,15 @@
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import AuthHandler from "@/handlers/AuthHandler";
 import { Outlet } from "react-router-dom";
 
 const PublicLayout = () => {
   return (
     <div className="w-full">
         {/* handler to store the user data*/}
+        <AuthHandler />
+        
         <Header />
 
         <Outlet />
